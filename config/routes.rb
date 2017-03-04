2f290b71086ffad7674854aa23d1a23bda330c65
John Messina <johnvm914@gmail.com>
2017-03-04 11:40:47 -0500

    cleaned up routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,9 @@ Rails.application.routes.draw do
 
   root to: "monuments#index"
 
-  get "/favorites", to: "favorites#index"
-
   namespace :api do
     namespace :v1 do
       resources :favorites, except: [:new, :edit]
-      # post "/favorites", to: "favorites#create"
     end
   end
 
